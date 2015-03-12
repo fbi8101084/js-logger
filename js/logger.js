9f3f8613e5bb35ab0616c9e2b1c05b26330c254e
nobita <nobita@pixnet.tw>
2015-03-12 11:18:34 +0800

    restyle

--- js/logger.js
+++ js/logger.js
@@ -2,15 +2,34 @@
     'use strict';
     var fg = fg || {};
     fg.js_error_type = fg.js_error_type || 'other';
-    fg.js_logger = function (message) {
-        if ('undefined' !== typeof fg.js_error_alert && fg.js_error_alert) {
-            alert(message);
+    if ('undefined' !== typeof console) {
+
+        fg.js_logger = function (message) {
+            if (fg.js_error_alert) {
+                alert(message);
+            }
+            if ('undefined' !== typeof console.log) {
+                console.log(message);
+            }
+            (new Image()).src =
+                '//error.fgplug.in/' + fg.js_error_type + '.gif' +
+                '?message=' + encodeURIComponent(message) +
+                '&from=' + encodeURIComponent(document.location);
+        };
+
+        if ('undefined' !== typeof console.error) {
+            console.error = function () {
+                var argument_strings = [], i;
+
+                for (i = 0; i < arguments.length; i++) {
+                    argument_strings.push(' ' + arguments[i]);
+                }
+
+                fg.js_logger('console.error: ' + argument_strings.join(' '));
+            };
         }
-        if ('undefined' !== typeof console && 'undefined' !== typeof console.log) {
-            console.log(message);
-        }
-        (new Image()).src = '//error.fgplug.in/' + fg.js_error_type + '.gif?message=' + encodeURIComponent(message) + '&from=' + encodeURIComponent(document.location);
-    };
+    }
+
     fg.window_error = function (errorMessage, url, line) {
         if ('object' == typeof errorMessage) {
             url = errorMessage.target.src;
@@ -19,26 +38,21 @@
         }
         fg.js_logger('window.onerror: ' + errorMessage + ' ' + url + ' ' + line);
     };
+
     if ('undefined' !== typeof window.addEventListener) {
         window.onerror = fg.window_error;
     } else if ('undefined' !== typeof window.attachEvent) {
         window.attachEvent('error', fg.window_error);
     }
-    jQuery(function () {
-        jQuery('<div>').ajaxError(function (event, request, settings, exception) {
-            fg.js_logger('AjaxError:' + settings.type + ' ' + settings.url + ' ' + request.status + ' (' + request.statusText + '):' + exception);
+
+    if ('undefined' !== typeof jQuery) {
+        jQuery(function () {
+            jQuery('<div>').ajaxError(function (event, request, settings, exception) {
+                fg.js_logger('AjaxError:' + settings.type + ' ' + settings.url + ' ' + request.status + ' (' + request.statusText + '):' + exception);
+            });
         });
-    });
-    jQuery.error = function (message) {
-        fg.js_logger('jQuery.error: ' + message);
-    };
-    if (typeof console != 'undefined' && typeof console.error != 'undefined') {
-        console.error = function () {
-            var argument_strings = [];
-            for (var i = 0; i < arguments.length; i++) {
-                argument_strings.push(' ' + arguments[i]);
-            }
-            fg.js_logger('console.error: ' + argument_strings.join(' '));
+        jQuery.error = function (message) {
+            fg.js_logger('jQuery.error: ' + message);
         };
     }
 })();
